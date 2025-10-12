2dbccfdb8b7505ef468fe8fbe495d86751cfd863
VaibhaV <vaibhav.codes09@gmail.com>
2025-10-12 15:36:59 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,84 @@
+// Selecting nodes;
+let form = document.querySelector("#toDoForm");
+let task = document.querySelector("#toDoInput");
+let displayTask = document.querySelector("#toDoList");
+let clearAllBtn = document.querySelector("#clearAllBtn");
+
+// Function for creating new task and adding to ul;
+function createAndAppend (text, completed){
+    // For New Task
+    let  newTask = document.createElement("li")
+    newTask.textContent = text;
+    newTask.dataset.toDoText = text;
+    
+    // For completion
+    if(completed){
+        newTask.classList.add('completed');
+    }
+    newTask.addEventListener('click', function(){
+        newTask.classList.toggle('completed');
+        updateStorage();
+    })
+
+    // For Deletion
+    let dltBtn = document.createElement('button');
+    dltBtn.textContent = 'Delete';
+    dltBtn.addEventListener('click', function(event){
+        event.stopPropagation();
+        event.target.parentElement.remove();
+        updateStorage();
+    })
+
+    displayTask.append(newTask);
+    newTask.append(dltBtn);
+}
+
+// For Clearing all Todos;
+clearAllBtn.addEventListener('click', function(){
+    displayTask.innerHTML = "";
+    localStorage.clear();
+})
+
+// Function for storing todos;
+function storing (text){
+    let userTask = JSON.parse(localStorage.getItem("userTask")) || [];
+    userTask.push({
+        text: text,
+        completed: false
+    });
+    localStorage.setItem("userTask", JSON.stringify(userTask));
+}
+
+// Function for updating localstorage
+function updateStorage (){
+    let allTasks = document.querySelectorAll('#toDoList li');
+    let updatedTasks = [];
+    
+    allTasks.forEach(function(item){
+        let toDoText = item.dataset.toDoText
+        updatedTasks.push({
+            text: toDoText,
+            completed: item.classList.contains('completed')
+        })
+    })
+    localStorage.setItem('userTask', JSON.stringify(updatedTasks));
+}
+
+// For Loading existing todos
+document.addEventListener("DOMContentLoaded", function(){
+    let savedToDos = JSON.parse(localStorage.getItem("userTask")) || [];
+    savedToDos.forEach(function(toDoTextObject){
+        createAndAppend(toDoTextObject.text, toDoTextObject.completed);
+    })
+})
+
+// Main Working
+form.addEventListener("submit", function(dets){
+    dets.preventDefault();
+    let userTask = task.value;
+    if(userTask  !== ""){
+        createAndAppend(userTask, false);
+        storing(userTask)
+        task.value = "" ;
+    }
+})
